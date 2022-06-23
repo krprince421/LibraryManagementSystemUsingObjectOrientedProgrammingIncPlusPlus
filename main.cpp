6b1df9c67dd2b558ca46b9f9f68268a181d4cb71
krprince421 <61113784+krprince421@users.noreply.github.com>
2022-06-23 22:30:28 +0530

    added feature of separate adding and finding book of cse and non-cse department

--- main.cpp
+++ main.cpp
@@ -10,13 +10,9 @@ string bookName , author ;
     // Otherwise get and set method should be called .
     Book(string bookName , string author , int bookId ,  int edition , int quantity ){
         this->bookName = bookName ;
-
         this->bookId = bookId ;
-
         this->author = author ;
-
         this->edition = edition ;
-
         this->quantity = quantity ;
 
 
@@ -103,20 +99,42 @@ public:
 class BookForNonCSE:public Book{
 int year , semester ;
 public:
- BookForNonCSE(){}
- BookForNonCSE( string bookName , string author , int bookId ,  int edition , int quantity , int year , int semester ):Book(bookName , author, bookId , edition, quantity ){
+    BookForNonCSE(){}
+    BookForNonCSE( string bookName , string author , int bookId ,  int edition , int quantity , int year , int semester ):Book(bookName , author, bookId , edition, quantity ){
 
         this->year = year ;
         this->semester = semester ;
+    }
+
+    bool setYear( int year ){
+        this->year = year ;
+        return true ;
+    }
+    int getYear(){
+        return this->year ;
+    }
+    bool setSemester( int semester ){
+        this->semester = semester ;
+        return true ;
+    }
+    int getSemester(){
+        return this->semester ;
+    }
+
+    void printNonCSEBookInformation(){
+        printBookInformation() ;
+        cout<<"Book Name : "<<getYear()<<endl;
+        cout<<"Book Id   : "<<getSemester()<<endl;
+
     }
 
 
 
 };
 class Person{
+string name ;
+char gender ;
 public:
-    string name ;
-    char gender ;
     Person(){}
     Person( string name , char gender ){
         this->name = name ;
@@ -134,12 +152,7 @@ public:
         this->batch = batch ;
         this->branchOfStudy = branchOfStudy ;
     }
-    void printStudentData(){
-    cout<<"Name   : "<<name<<endl;
-    cout<<"Gender : "<<gender<<endl;
-    cout<<"Batch  : "<<batch<<endl;
-    cout<<"BranchOfStudy:"<<branchOfStudy<<endl;
-    }
+
 };
 class Employee:public Person{
 int empId ;
@@ -149,12 +162,7 @@ public:
         this->empId = empId ;
         this->department = department ;
     }
-    void printEmployeeData(){
-        cout<<"Name        :"<<name<<endl;
-        cout<<"Gender      : "<<gender<<endl;
-        cout<<"Employee Id :"<<empId<<endl;
-        cout<<"Department  : "<<department<<endl;
-    }
+
 };
 
 int main(){
@@ -162,6 +170,7 @@ int main(){
 //BookForCSE *book1 =  new BookForCSE("Fundamental of computer Algorithm" , "Horowitz,Ellis" , 1000 , 2 , 50 , 2 , 2 ) ;
 
 BookForCSE *CSEbookArray = new BookForCSE[100] ;
+BookForNonCSE *NonCSEbookArray = new BookForNonCSE[100] ;
 //Book that is already available in library is added at index 0
 CSEbookArray[0].setBookName("Fundamental of computer Algorithm") ;
 CSEbookArray[0].setAuthor("Horowitz,Ellis") ;
@@ -171,63 +180,111 @@ CSEbookArray[0].setQuantity(50) ;
 CSEbookArray[0].setYear(2) ;
 CSEbookArray[0].setSemester(2) ;
 
+NonCSEbookArray[0].setBookName("Electronics and Communication Engineering Handbook") ;
+NonCSEbookArray[0].setAuthor(" Susurla V.S. Suresh") ;
+NonCSEbookArray[0].setId(2000) ;
+NonCSEbookArray[0].setEdition(1) ;
+NonCSEbookArray[0].setQuantity(55) ;
+NonCSEbookArray[0].setYear(1) ;
+NonCSEbookArray[0].setSemester(2) ;
 
+//NonCSEbookArray[0].printNonCSEBookInformation() ;
 cout<<"Welcome to Library Management System "<<endl ;
 cout<<"Press 1 if you want save information of new book "<<endl ;
 cout<<"Press 2 if you want to know information about existing book "<<endl ;
 cout<<"Enter 404 to finish the programme "<<endl;
-int userInput ;
-
-int indexOfLastBook = 1 ;
-while( true ){
+int userInput = 1 ;
+int indexCSE = 1 , indexNonCSE = 1 ;
+while( userInput == 1 || userInput == 2 ){
     string bookName , author ;
     int bookId , edition , quantity , year , semester ;
     cin>>userInput ;
-
     if( userInput == 1 ){
-// Read and set the value of new book
-    cout<<"Book Name ";
-    cin>>bookName ;
-    CSEbookArray[indexOfLastBook].setBookName( bookName ) ;
-    cout<<"Book Id ";
-    cin>>bookId ;
-    CSEbookArray[indexOfLastBook].setId( bookId ) ;
-    cout<<"Book Author " ;
-    cin>>author ;
-    CSEbookArray[indexOfLastBook].setAuthor( author ) ;
-    cout<<"Book Edition " ;
-    cin>>edition ;
-    CSEbookArray[indexOfLastBook].setEdition( edition ) ;
-    cout<<"Book Quantity " ;
-    cin>>quantity ;
-    CSEbookArray[indexOfLastBook].setQuantity( quantity ) ;
-    cout<<"Year of book " ;
-    cin>>year ;
-    CSEbookArray[indexOfLastBook].setYear( year ) ;
-    cout<<"Semester of book " ;
-    cin>>semester ;
-    CSEbookArray[indexOfLastBook].setSemester( semester ) ;
+        cout<<"Enter 1 if you want to add book of CSE Branch"<<endl;
+        cout<<"Enter 2 if you want to add book of Non-CSE Branch"<<endl;
+        int bookBranch ;
+        cin>>bookBranch ;
+        if( bookBranch == 1 ){
+            // Read and set the value of new CSE book
+            cout<<"Book Name ";
+            cin>>bookName ;
+            CSEbookArray[indexCSE].setBookName( bookName ) ;
+            cout<<"Book Id ";
+            cin>>bookId ;
+            CSEbookArray[indexCSE].setId( bookId ) ;
+            cout<<"Book Author " ;
+            cin>>author ;
+            CSEbookArray[indexCSE].setAuthor( author ) ;
+            cout<<"Book Edition " ;
+            cin>>edition ;
+            CSEbookArray[indexCSE].setEdition( edition ) ;
+            cout<<"Book Quantity " ;
+            cin>>quantity ;
+            CSEbookArray[indexCSE].setQuantity( quantity ) ;
+            cout<<"Year of book " ;
+            cin>>year ;
+            CSEbookArray[indexCSE].setYear( year ) ;
+            cout<<"Semester of book " ;
+            cin>>semester ;
+            CSEbookArray[indexCSE].setSemester( semester ) ;
 
-    //Print the information of the new book added right now
+            //Print the information of the new book added right now
+            cout<<"Book Added Successfully with information"<<endl;
+            CSEbookArray[indexCSE].printCSEBookInformation() ;
+            indexCSE++ ;
+        }else if( bookBranch == 2 ){
 
-    CSEbookArray[indexOfLastBook].printCSEBookInformation() ;
+            // Read and set the value of new CSE book
+            cout<<"Book Name ";
+            cin>>bookName ;
+            NonCSEbookArray[indexNonCSE].setBookName( bookName ) ;
+            cout<<"Book Id ";
+            cin>>bookId ;
+            NonCSEbookArray[indexNonCSE].setId( bookId ) ;
+            cout<<"Book Author " ;
+            cin>>author ;
+            NonCSEbookArray[indexNonCSE].setAuthor( author ) ;
+            cout<<"Book Edition " ;
+            cin>>edition ;
+            NonCSEbookArray[indexNonCSE].setEdition( edition ) ;
+            cout<<"Book Quantity " ;
+            cin>>quantity ;
+            NonCSEbookArray[indexNonCSE].setQuantity( quantity ) ;
+            cout<<"Year of book " ;
+            cin>>year ;
+            NonCSEbookArray[indexNonCSE].setYear( year ) ;
+            cout<<"Semester of book " ;
+            cin>>semester ;
+            NonCSEbookArray[indexNonCSE].setSemester( semester ) ;
 
-    indexOfLastBook++ ;
+            //Print the information of the new book added right now
+            cout<<"Book Added successfully with information "<<endl;
+            NonCSEbookArray[indexNonCSE].printNonCSEBookInformation() ;
+            indexNonCSE++ ;
+
+
+        }
+
     }
     else if( userInput == 2 ){
         cout<<"Enter the id of book you want to find " ;
         int idOfBookToFind ;
         cin>>idOfBookToFind ;
         bool bookFound = false  ;
-        for( int i = 0 ; i < indexOfLastBook ; i++ ){
+        for( int i = 0 ; i < indexCSE ; i++ ){
             if( CSEbookArray[i].getId() == idOfBookToFind ){
                 bookFound = true ;
                 CSEbookArray[i].printCSEBookInformation() ;
             }
         }
+        for( int i = 0 ; i < indexNonCSE ; i++ ){
+            if( NonCSEbookArray[i].getId() == idOfBookToFind ){
+                bookFound = true ;
+                NonCSEbookArray[i].printNonCSEBookInformation() ;
+            }
+        }
         if(bookFound == false )cout<<"Book Not Found with id : "<<idOfBookToFind<<endl;
     }
-    else break ;
 
 }
 
